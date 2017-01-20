760d47f6708c970049969fec4fe974064c8062bd
FTAndy <fortheandy@gmail.com>
2017-01-20 18:31:13 +0800

    add some function

---
+++ mimi-eventemitter.js
@@ -0,0 +1,30 @@
+module.exports = MimiEventEmitter
+
+function MimiEventEmitter() {
+  this.listeners = new Map()
+  this.maxListeners = 10
+
+  const getListener = (eventName) => {
+
+  }
+
+  const addListener = (eventName, fn) => {
+
+  }
+
+  const removeListener = (eventName, fn) => {
+
+  }
+
+  const trigger = (event, ...args) => {
+
+  }
+
+  const getAccount = (eventName) => {
+
+  }
+
+  const once = (eventName, fn) => {
+
+  }
+}
